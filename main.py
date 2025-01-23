158a92d4fc5dbad1eb8911c2f824799dc43835fe
lidagm <lida_mann@biswashington.org>
2025-01-23 14:56:44 +0000

    hello

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Does this work?")
+with open("students.txt","w") as file:
+    file.write("Lida\nTara\nOliver G\nJoseph\nBram\nLetotlo\nBen\nAlessandro\nLayla\nMarlee\nTao\nSofi\nSelma\nAmelia\nLeila\nOliver M\nHarry\nAxel\nBayne\nMaisie\nJolie\nRafael")
+with open("students.txt","a") as file:
+    new_line = input("Enter the line you want to add to the file:")
+    file.write(new_line + "\n")
+with open("students.txt","r") as file:
+    for line in file:
+        print(line.strip())
+        print(line[1])
+
+with open("students.txt","r") as file:
+    for line in file:
+        if len(line)>6:
+            print(line)
+    
